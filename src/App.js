88ece3322550dbf32313e62e88baaf10eda345af
Alex Maior <alexmaior@yahoo.com>
2021-10-22 23:25:15 +0300

    Fetched the data from the API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import React, { useEffect, useState } from "react";
+
 function App() {
-  return <div className="App">Hello World</div>;
+  const [weather, setWeather] = useState([]);
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    const getWeather = async () => {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=7f081dfa72e4bdbf2710be42a5a231c3`
+      );
+      const data = await response.json();
+      console.log(data);
+      setWeather(data);
+    };
+    getWeather();
+  }, [query]);
+
+  return (
+    <div className="App">
+      <div className="recipes">{weather.dt}</div>
+    </div>
+  );
 }
 
 export default App;
